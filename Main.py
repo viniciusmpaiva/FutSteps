c8153719256811eb4e8c6fc0a65e3fed811fafdb
viniciusmpaiva <vinimpaiva@gmail.com>
2024-12-07 18:02:07 -0300

    insertions done

--- Main.py
+++ Main.py
@@ -1,8 +1,5 @@
 # from Modules.Connection import Connection
 from Modules.Interface import Interface
-# connection = Connection.connectToDB()
-# cursor = connection.cursor();
 interface = Interface()
 while True:
     interface.start()
-    
--- Modules/DBFunctions.py
+++ Modules/DBFunctions.py
@@ -1,13 +1,11 @@
+
 class DBFunctions:
     def insert(table, data, connection):
-        try:
-            cursor = connection.cursor()
-            placeholders = ", ".join(["%s" for _ in data])
-            query = f'INSERT INTO {table} VALUES ({placeholders})'
-            cursor.execute(query, data)
-            connection.commit()
-            return 200
-        except Exception as e :
-            return e
-        
+        cursor = connection.cursor()
+        placeholders = ", ".join(["%s" for _ in data])
+        query = f'INSERT INTO {table} VALUES ({placeholders})'
+        cursor.execute(query, data)
+        connection.commit()
+    
+
         
